dccab39326692dc5ef301ba4082be1555d1cfe35
Giacomo Giraldi <ggiraldi18@gmail.com>
2023-12-19 20:45:22 +0100

    fix sidebar

---
+++ src/component/nav/MiniFooter.jsx
@@ -0,0 +1,22 @@
+import LanguageModal from "../modal/LanguageModal.jsx";
+export default function MiniFooter(){
+    return(
+        <div
+        className="mini-footer w-[430px] ml-2 fixed bottom-0 text-white p-10 text-sm rounded-2xl"
+        style={{ backgroundColor: "rgba(18,18,18,255)" }}
+      >
+        <div className="first-line flex opacity-70 gap-6 p-2">
+          <p>Legal</p>
+          <p>Privacy Center</p>
+          <p>Privacy Policy</p>
+        </div>
+        <div className="second-line flex opacity-70 gap-6 p-2">
+          <p>Cookie Settings</p>
+          <p>About Ads</p>
+          <p>Accessibility</p>
+        </div>
+        <p className="opacity-70 p-2">Cookies</p>
+        <LanguageModal />
+      </div>
+    )
+}
--- src/pages/Searchpage.jsx
+++ src/pages/Searchpage.jsx
@@ -2,22 +2,24 @@ import Search from "../component/section/Search";
 import Sidebar from "../component/nav/Sidebar";
 import { Link } from "react-router-dom";
 import NavwithSearch from "../component/common/NavwithSearch";
+import MiniFooter from "../component/nav/MiniFooter";
 export default function Searchpage() {
-   
   return (
     <div>
-        <div className='flex justify-between '>
-          <div className=" w-[300px]"><Sidebar /></div>
-        <div className='w-[100%]'>
-          <NavwithSearch/>
-        <div >
-        <div className='flex-col justify-start'>
-          <Search/>
+      <div className="flex justify-between ">
+        <div className=" w-[50vw]sticky">
+          <Sidebar />
         </div>
+        <div className="">
+          <NavwithSearch />
+          <div>
+            <div className="">
+              <Search />
+            </div>
+          </div>
         </div>
-        
-        </div>
+      </div>
+      <MiniFooter />
     </div>
-    </div>
-  )
+  );
 }
